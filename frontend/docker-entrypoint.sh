9cd9293cec9d366255d0b8cc511bee135a0ad6ee
vivamau <maurizio.blasilli@gmail.com>
2026-04-01 14:28:11 +0200

    remove anything docker

--- frontend/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Substitute only BACKEND_URL in nginx config, preserving other nginx variables like $uri
-envsubst '${BACKEND_URL}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
-
-# Start Nginx
-exec nginx -g 'daemon off;'
